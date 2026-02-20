2247101990c4f2173e847cf21c0fb8beac006139
KavinPratap <kavin.pratap@gmail.com>
2026-02-20 18:26:06 +0000

    sdds

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+
+
+int main(){
+    printf("hello\n\n\n");
+    
+    int spacing = 10;
+    for(int count = 0; count < 10;count++) {
+        
+        for (int count2; count2 < 10; i++)
+        {
+            /* code */
+        }
+        
+    }
+
+
+    return 0;
+}
+
